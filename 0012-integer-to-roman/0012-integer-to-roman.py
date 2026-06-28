e706922fdbed1c32a8e0fe8cebe9888647e5a281
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-06-28 13:14:29 +0530

    Time: 50 ms (5.85%), Space: 19.2 MB (90.51%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,30 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman = [
+        { "value": 1000, "symbol": 'M' },
+        { "value": 900, "symbol": 'CM' },
+        { "value": 500, "symbol": 'D' },
+        { "value": 400, "symbol": 'CD' },
+        { "value": 100, "symbol": 'C' },
+        { "value": 90, "symbol": 'XC' },
+        { "value": 50, "symbol": 'L' },
+        { "value": 40, "symbol": 'XL' },
+        { "value": 10, "symbol": 'X' },
+        { "value": 9, "symbol": 'IX' },
+        { "value": 5, "symbol": 'V' },
+        { "value": 4, "symbol": 'IV' },
+        { "value": 1, "symbol": 'I' }
+    ]
+        res = []
+        for sym in roman:
+            print(sym, sym.get('value'), sym.get('symbol'))
+            while num >= sym.get('value'):
+                res.append(sym.get('symbol'))
+                num -= sym.get('value')
+        print(res)
+        return "".join(res)
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
