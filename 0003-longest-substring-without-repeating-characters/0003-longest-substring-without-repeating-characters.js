c858630cda10eea894fe7788a8852ff3f31c8d44
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-06-28 13:07:35 +0530

    Time: 16 ms (26.53%), Space: 59 MB (42.42%) - LeetHub - Time: O(n), Space: O(min(n, m))

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    const last_seen_index = {}
+
+    let window_start = 0;
+
+    let max_length = 0;
+
+    for(let window_end = 0; window_end<s.length; window_end++){
+        if ( s[window_end] in last_seen_index && last_seen_index[s[window_end]] >= window_start){
+            window_start = last_seen_index[s[window_end]] + 1
+        }
+
+        last_seen_index[s[window_end]] = window_end
+
+        max_length = Math.max(max_length, window_end - window_start + 1)
+    }
+    return max_length
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
