8425e86af5cefe2ffbb5fd280686bf13ca04cd6e
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-06-29 17:00:58 +0530

    Time: 16 ms (25.79%), Space: 57.3 MB (40.27%) - LeetHub - Time: O(n^2), Space: O(1)

---
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+    nums.sort((a, b) => a - b)
+
+    nearest = nums[0] + nums[1] + nums[2]
+    for(let i = 0; i<nums.length; i++){
+        if (i > 0 && nums[i] == nums[i - 1]) {continue}
+
+        let start = i + 1
+        let end = nums.length - 1
+
+        while (start < end){
+            total = nums[i] + nums[start] + nums[end]
+
+            if (Math.abs(target - total) < Math.abs(target - nearest)){
+                nearest = total
+            }
+            if (total === target){
+                return total
+            } else if (total < target){
+                start += 1
+            } else {
+                end -= 1
+            }
+        }
+    }
+    return nearest
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
