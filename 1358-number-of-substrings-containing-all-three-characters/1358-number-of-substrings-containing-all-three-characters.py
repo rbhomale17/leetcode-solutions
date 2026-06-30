8bf75776afdc40ae5520099375c53c17fa6804a5
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-06-30 22:17:57 +0530

    Time: 77 ms (77.8%), Space: 19.6 MB (15.25%) - LeetHub - Time: O, Space: O(?)

---
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        res = 0
+        n = len(s)
+
+        last_seen = {"a": -1, "b": -1, "c": -1}
+        # {"a": 0, "b": 1, "c": 2}
+        for i in range(n):
+            last_seen[s[i]] = i
+            if (
+            last_seen["a"] != -1 
+            and last_seen["b"] != -1 
+            and last_seen["c"] != -1
+            ):
+                left = min(last_seen["a"], last_seen["b"], last_seen["c"])
+                res += left + 1
+                # res = 1
+        return res
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
