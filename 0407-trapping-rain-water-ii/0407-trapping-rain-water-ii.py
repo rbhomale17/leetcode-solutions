1882ab6e1eaacf7b94b861c32156da87d40061a2
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-06-30 15:45:01 +0530

    Time: 122 ms (34.64%), Space: 54.9 MB (6.14%) - LeetHub - Time: O(m*n*log(m*n)), Space: O(m*n)

---
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def trapRainWater(self, heightMap):
+        def rec(i, j, i1, j1):
+            tot = 0
+            pending.remove((i1, j1))
+            counted.add((i1, j1))
+            tot += heightMap[i][j] - heightMap[i1][j1]
+            if (i1 - 1, j1) in pending:
+                tot += rec(i, j, i1 - 1, j1)
+            if (i1, j1 + 1) in pending:
+                tot += rec(i, j, i1, j1 + 1)
+            if (i1 + 1, j1) in pending:
+                tot += rec(i, j, i1 + 1, j1)
+            if (i1, j1 - 1) in pending:
+                tot += rec(i, j, i1, j1 - 1)
+            return tot
+        m, n = len(heightMap), len(heightMap[0])
+        pending, counted = set(), set()
+        tot = 0
+        pts = sorted([(i, j) for i in range(m) for j in range(n)], key=lambda a: heightMap[a[0]][a[1]])
+        for i, j in pts:
+            pending.add((i, j))
+            if i == 0 or i == m - 1 or j == 0 or j == n - 1 or \
+                (i - 1, j) in counted or \
+                (i, j + 1) in counted or \
+                (i + 1, j) in counted or \
+                (i, j - 1) in counted:
+                stk = [(i, j)]
+                tot += rec(i, j, i, j)
+        return tot
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
