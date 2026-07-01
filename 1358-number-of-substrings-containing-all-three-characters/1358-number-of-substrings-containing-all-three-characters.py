59de15a3e8bde300696f5fc77a7176ab4b6467c7
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-07-01 14:37:34 +0530

    Time: 71 ms (84.09%), Space: 19.4 MB (48.32%) - LeetHub - Time: O(n), Space: O(1)

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.py
@@ -18,3 +18,6 @@ class Solution:
         return res
 
 
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
