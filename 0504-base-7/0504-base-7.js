aba7689a17335abcf1ab50c6b12c501f4a6097b0
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-07-01 10:55:31 +0530

    Time: 0 ms (100%), Space: 53.4 MB (74.22%) - LeetHub - Time: O(log(n)), Space: O(log(n))

---
+++ 0504-base-7/0504-base-7.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var convertToBase7 = function(num) {
+    return num.toString(7);
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
