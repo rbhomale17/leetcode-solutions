0231352322d8b52c5c4a3886d2c7720263bc7080
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-07-05 21:43:40 +0530

    Time: 9 ms (66.35%), Space: 19.3 MB (77.28%) - LeetHub - Time: O(?), Space: O(?)

--- 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.py
+++ 3982-sum-of-integers-with-maximum-digit-range/3982-sum-of-integers-with-maximum-digit-range.py
@@ -22,3 +22,6 @@ class Solution:
         
         return res
 
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
