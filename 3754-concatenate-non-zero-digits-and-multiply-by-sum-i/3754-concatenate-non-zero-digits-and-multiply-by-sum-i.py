3f061c62d067400d727171d333f92e3a84a120d5
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-07-07 10:02:48 +0530

    Time: 2 ms (27.7%), Space: 19.4 MB (14.68%) - LeetHub - Time: O(log n), Space: O(log n)

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sumAndMultiply(self, n: int) -> int:
+        x = [int(num) for num in str(n) if int(num) > 0]
+        # print(x, type(x))
+
+        x_num = 0
+        x_sum = 0
+        i = 0
+        while i < len(x):
+            digit = x[i]
+            x_num = (x_num * 10) + digit
+            x_sum += digit
+        
+            i += 1
+        
+        # print(x_num, x_sum)
+        return x_num * x_sum
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
