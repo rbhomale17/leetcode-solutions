c68005f1c70a793c28aae8f768c56a9b978e690d
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-07-08 16:25:55 +0530

    Time: 844 ms (6.62%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -10,6 +10,13 @@ FROM (
 ) as nums
 WHERE nums.num = nums.num1 and nums.num = nums.num2
 
+
+-- SELECT DISTINCT l1.num as ConsecutiveNums
+-- FROM Logs l1, logs l2, logs l3
+-- WHERE
+--     l1.id = l2.id - 1 AND l1.id = l3.id - 2
+--     AND l1.num = l2.num AND l1.num = l3.num
+
 -- Synced seamlessly with LeetHub Pro
 -- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
 -- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
