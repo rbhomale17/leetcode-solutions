470b2d98ad1e1e3ce925bf464b6950e5dde402b1
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-07-08 16:49:45 +0530

    Time: 513 ms (97.47%), Space: 0B (100%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT
+    date_id,
+    make_name,
+    COUNT(DISTINCT lead_id) as unique_leads,
+    COUNT(DISTINCT partner_id) as unique_partners
+FROM
+    DailySales
+GROUP BY date_id, make_name
+ORDER BY date_id
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
