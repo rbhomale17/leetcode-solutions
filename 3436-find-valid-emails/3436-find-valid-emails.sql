bec998391703e4a848afb970b62dc039cc70ebe3
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-07-10 11:03:17 +0530

    Time: 457 ms (67.91%), Space: 0B (100%) - LeetHub - Time: O(n log n), Space: O(n)

--- 3436-find-valid-emails/3436-find-valid-emails.sql
+++ 3436-find-valid-emails/3436-find-valid-emails.sql
@@ -4,7 +4,7 @@ SELECT
 FROM
     Users
 WHERE
-    REGEXP_LIKE(email, '^[A-Za-z0-9_]+@[A-Za-z]+\\.com$')
+    email REGEXP '^[A-Za-z0-9_]+@[A-Za-z]+\\.com$'
 ORDER BY user_id
 
 -- Synced seamlessly with LeetHub Pro
