1c4320e393e8483baa49ac7894368d9be2efef2b
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-07-23 11:20:53 +0530

    Time: 291 ms (71%), Space: 0B (100%) - LeetHub - Time: O(n*m), Space: O(n)

--- 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
+++ 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
@@ -1,33 +1,51 @@
 # Write your MySQL query statement below
 SELECT
     *,
+    (dna_sequence LIKE 'ATG%') as has_start,
     (
-        CASE
-            WHEN dna_sequence LIKE 'ATG%' THEN 1
-            ELSE 0
-        END
-    ) as has_start,
-    (
-        CASE
-            WHEN dna_sequence LIKE '%TAA' or dna_sequence LIKE '%TAG' or dna_sequence LIKE '%TGA' THEN 1
-            ELSE 0
-        END
+        dna_sequence LIKE '%TAA' 
+        or dna_sequence LIKE '%TAG' 
+        or dna_sequence LIKE '%TGA'
     ) as has_stop,
-    (
-        CASE
-            WHEN dna_sequence LIKE '%ATAT%' THEN 1
-            ELSE 0
-        END
-    ) as has_atat,
-    (
-        CASE
-            WHEN dna_sequence LIKE '%GGG%' THEN 1
-            ELSE 0
-        END
-    ) as has_ggg
+    (dna_sequence LIKE '%ATAT%') as has_atat,
+    (dna_sequence LIKE '%GGG%') as has_ggg
 FROM
     samples
 ORDER BY sample_id ASC
+
+
+
+
+-- # Write your MySQL query statement below
+-- SELECT
+--     *,
+--     (
+--         CASE
+--             WHEN dna_sequence LIKE 'ATG%' THEN 1
+--             ELSE 0
+--         END
+--     ) as has_start,
+--     (
+--         CASE
+--             WHEN dna_sequence LIKE '%TAA' or dna_sequence LIKE '%TAG' or dna_sequence LIKE '%TGA' THEN 1
+--             ELSE 0
+--         END
+--     ) as has_stop,
+--     (
+--         CASE
+--             WHEN dna_sequence LIKE '%ATAT%' THEN 1
+--             ELSE 0
+--         END
+--     ) as has_atat,
+--     (
+--         CASE
+--             WHEN dna_sequence LIKE '%GGG%' THEN 1
+--             ELSE 0
+--         END
+--     ) as has_ggg
+-- FROM
+--     samples
+-- ORDER BY sample_id ASC
 
 
 -- Synced seamlessly with LeetHub Pro
