7e478546cb265a442b3aa0fd97e8cec3e4f288d3
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-07-23 11:17:07 +0530

    Time: 314 ms (36.03%), Space: 0B (100%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 3475-dna-pattern-recognition/3475-dna-pattern-recognition.sql
@@ -0,0 +1,35 @@
+# Write your MySQL query statement below
+SELECT
+    *,
+    (
+        CASE
+            WHEN dna_sequence LIKE 'ATG%' THEN 1
+            ELSE 0
+        END
+    ) as has_start,
+    (
+        CASE
+            WHEN dna_sequence LIKE '%TAA' or dna_sequence LIKE '%TAG' or dna_sequence LIKE '%TGA' THEN 1
+            ELSE 0
+        END
+    ) as has_stop,
+    (
+        CASE
+            WHEN dna_sequence LIKE '%ATAT%' THEN 1
+            ELSE 0
+        END
+    ) as has_atat,
+    (
+        CASE
+            WHEN dna_sequence LIKE '%GGG%' THEN 1
+            ELSE 0
+        END
+    ) as has_ggg
+FROM
+    samples
+ORDER BY sample_id ASC
+
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
