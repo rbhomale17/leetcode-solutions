81be7458e190d48f13be0bc7022b6ff7a65c8250
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-07-28 10:16:23 +0530

    Time: 560 ms (24.63%), Space: 0B (100%) - LeetHub - Time: O(N log N), Space: O(N)

---
+++ 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+SELECT
+    activity_date as day,
+    COUNT(
+        DISTINCT(user_id)
+        -- CASE
+        --     WHEN activity_type IS NOT NULL
+    ) as active_users
+FROM
+    Activity
+WHERE activity_date > DATE_SUB('2019-07-27', INTERVAL 30 DAY) 
+    AND activity_date <= '2019-07-27'
+GROUP BY activity_date
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
