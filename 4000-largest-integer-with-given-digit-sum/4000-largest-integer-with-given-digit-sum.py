0c32a22949f8b93fb207b87d32e11f3332068e4b
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-07-30 11:36:43 +0530

    Time: 298 ms (13.63%), Space: 19.5 MB (5.11%) - LeetHub - Time: O(10^n * n), Space: O(n)

---
+++ 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def largestInteger(self, n: int, s: int) -> int:
+        if s > n * 9:
+            return -1
+        elif s == 0:
+            return 0
+        
+        digits = 0
+        while n > 0:
+            digits = digits * 10 + 9
+            n = n - 1
+
+        while digits > 0 :
+            if sum(int(x) for x in str(digits)) == s:
+                return digits
+
+            digits = digits - 1
+
+        return -1       
+
+
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
