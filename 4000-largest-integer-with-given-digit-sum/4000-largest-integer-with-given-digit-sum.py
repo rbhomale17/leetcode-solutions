468993633b8aeec5f86ab0815a2bae882b0d57c1
Rushikesh Diliprao Bhomale <121092445+rbhomale17@users.noreply.github.com>
2026-07-30 11:58:26 +0530

    Time: 0 ms (100%), Space: 19.2 MB (87.82%) - LeetHub - Time: O(n), Space: O(n)

--- 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.py
+++ 4000-largest-integer-with-given-digit-sum/4000-largest-integer-with-given-digit-sum.py
@@ -5,19 +5,59 @@ class Solution:
         elif s == 0:
             return 0
         
-        digits = 0
-        while n > 0:
-            digits = digits * 10 + 9
-            n = n - 1
+        num_string = ''
 
-        while digits > 0 :
-            if sum(int(x) for x in str(digits)) == s:
-                return digits
+        for x in range(n):
+            curr_digit = min(s, 9)
+            num_string += str(curr_digit)
+            s -= curr_digit
 
-            digits = digits - 1
+        # print(s, num_string) 
+        if s != 0:
+            return -1
+        
+        return int(num_string)
 
-        return -1       
+"""
+Dry run N = 3, S = 15
+
+if 15 > 3 * 9 False 
+if 15 == 0 False
+
+num_string = ""
+
+loop start till N ie. 3 times
+
+Iter 1:
+s = 15
+num_string = ""
+curr_digit = min(s=15, 9) = 9
+num_string = num_string + curr_digit = "" + 9 = "9"
+s = s - curr_digit = 15 - 9 = 6
+
+Iter 2:
+s = 6
+num_string = "9"
+curr_digit = MIN(s=6, 9) = 6
+num_string = num_string + curr_digit = "9" + "6" = "96"
+s = s - curr_digit = 6 - 6 = 0
+
+Iter 3:
+s = 0
+num_string = "96"
+curr_digit = MIN(s=0, 9) = 0
+num_string = num_string + curr_digit = "96" + "0" = "960"
+s = s - curr_digit = 0 - 0 = 0
+
+loop compleated 
+num_string = "960"
+s = 0
+
+if s != 0: return -1
+else return int(num_string)
+"""
 
 
-
-
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
